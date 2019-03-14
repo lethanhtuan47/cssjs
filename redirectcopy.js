19ef2f8ed7099b677770b676ed222f91e3410133
lethanhtuan47 <thanhtuan8710@gmail.com>
2019-03-14 15:12:12 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Muốn Copy Thì Vào Đây: <a href='http://canhohungthinh.org/'>http://canhohungthinh.org/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Muốn Copy Thì Vào Đây: <a href='http://canhohungthinh.org/'>http://canhohungthinh.org"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
