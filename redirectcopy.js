96ec3a78ced7074f246cba1b58d7c25e8e4e5bce
lethanhtuan47 <thanhtuan8710@gmail.com>
2019-03-18 16:56:22 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -15,3 +15,23 @@ function FSAddOriginalLink(){
     },0);
 }
 document.oncopy = FSAddOriginalLink;
+
+
+
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/>MUỐN COPY VÀO ĐÂY : <a href='http://datnennhapho24h.com'>http://datnennhapho24h.com"+document.location.pathname+"</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
